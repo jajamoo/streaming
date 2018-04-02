03e174ad497d7464946b7e456c63406a6230a87b
MoeSymDistro <esanul@symdistro.com>
2018-04-02 10:02:34 -0400

    asdas

--- streaming_data/stream.py
+++ streaming_data/stream.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-local_file = '/sms_estimates_summary.csv'
+local_file = 'sms_estimates_summary.csv'
 
 
 def get_data(index_col='date'):
 
     data = pd.read_csv(
         local_file,
-        index_col = 'report_date',
         usecols = ['label_id', 'release_id', 'sum_units', 'sum_royalty', 'report_date', 'territory_id'],
         parse_dates = ['report_date']
     )
